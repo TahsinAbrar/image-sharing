9d6afe23f6e1ffb666a6be770468ef6a815b6a31
Tahsin Abrar <abrar.cste@gmail.com>
2014-10-26 09:51:16 +0600

    image configuration file created in app/config folder

---
+++ app/config/image.php
@@ -0,0 +1,13 @@
+<?php
+
+return array(
+    //the folder that will hold original uploaded images
+    'upload_folder' => 'uploads',
+    //the folder that will hold thumbnails
+    'thumb_folder' => 'uploads/thumb',
+    //width of the resized thumbnail
+    'thumb_width' => 320,
+    //height of the resized thumbnail
+    'thumb_height' => 240
+
+);
