09009d880bae69d2f7842c7f9f51fb6cca48eb5f
Tahsin Abrar <abrar.cste@gmail.com>
2014-10-27 15:59:58 +0600

    intervention/image setup to the laravel framework

--- app/config/image.php
+++ app/config/image.php
@@ -2,9 +2,9 @@
 
 return array(
     //the folder that will hold original uploaded images
-    'upload_folder' => 'uploads',
+    'upload_folder' => public_path().'\\uploads\\',
     //the folder that will hold thumbnails
-    'thumb_folder' => 'uploads/thumb',
+    'thumb_folder' => public_path().'\\uploads\\thumb\\',
     //width of the resized thumbnail
     'thumb_width' => 320,
     //height of the resized thumbnail
