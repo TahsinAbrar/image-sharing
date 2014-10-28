d14a3ecc86ffbb11727c3a4fd479c484573cee58
Tahsin Abrar <abrar.cste@gmail.com>
2014-10-28 10:14:00 +0600

    showing the image link within the input field

--- app/views/board/permalink.blade.php
+++ app/views/board/permalink.blade.php
@@ -11,15 +11,17 @@
 </div>
 <br/>
 <h6>Direct Image URL: </h6>
-<p> {{ URL::to(Config::get('image.upload_folder').$image->image) }}</p>
+<p>
+<input type="text" value="{{ URL::to(Config::get('image.upload_folder').$image->image) }}"/>
+</p>
 <br/>
 <h5>Thumbnail Forum BBCode </h5>
-[url]= {{ URL::to('snatch/'.$image->id) }}][img]{{ URL::to(Config::get('image.upload_folder').'/'.$image->image) }}[/img][/url]
+<input type="text" value="[url]= {{ URL::to('snatch/'.$image->id) }}][img]{{ URL::to(Config::get('image.upload_folder').'/'.$image->image) }}[/img][/url]"/>
 
 <br/>
 <h5>Thumbnail HTML Code: </h5>
 <p>
-{{ HTML::entities(HTML::link(URL::to('snatch/'.$image->id)),HTML::image(Config::get('image.upload_folder').'/'.$image->image) ) }}
+<input type="text" value="{{ HTML::entities(HTML::link(URL::to('snatch/'.$image->id)),HTML::image(Config::get('image.upload_folder').'/'.$image->image) ) }}"/>
 </p>
 
 @stop
